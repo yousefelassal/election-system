319dc1e6960d2cf0b0a61465ae56f5e864cc82ac
Yousef <yousefsameh24@gmail.com>
2024-05-25 20:09:14 +0300

    Refactor Signup page styling and add form validation

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -33,7 +33,7 @@ const Signup = () => {
     }
   }
   return (
-    <div className="flex flex-col gap-8 items-center justify-center w-full">
+    <div className="flex flex-col min-h-screen gap-8 items-center w-full">
         <header className="flex w-full items-center justify-between lg:border-b p-4 lg:px-8">
             <Link to="/" className="group flex items-center justify-center gap-1">
                 <svg className="lg:hidden" xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 39 39" fill="none">
@@ -62,8 +62,12 @@ const Signup = () => {
                 </Link>
             </div>
         </header>
-        <form onSubmit={form.handleSubmit(handleLogin)} className="flex w-full flex-col gap-4 p-4 lg:px-8">
-            <h1 className="font-bold text-lg">Sign Up</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44" fill="none">
+            <path d="M23 0L23.823 3.36707C25.109 8.62855 25.752 11.2593 27.1233 13.3821C28.336 15.2593 29.9527 16.8418 31.8554 18.0139C34.0071 19.3395 36.651 19.926 41.9388 21.0991L46 22L41.9388 22.9009C36.651 24.074 34.0071 24.6605 31.8554 25.9861C29.9527 27.1582 28.336 28.7407 27.1233 30.6179C25.752 32.7407 25.109 35.3714 23.823 40.6329L23 44L22.177 40.6329C20.891 35.3714 20.248 32.7407 18.8767 30.6179C17.664 28.7407 16.0473 27.1582 14.1446 25.9861C11.9929 24.6605 9.34898 24.074 4.06116 22.9009L0 22L4.06116 21.0991C9.34897 19.926 11.9929 19.3395 14.1446 18.0139C16.0473 16.8418 17.664 15.2593 18.8767 13.3821C20.248 11.2593 20.891 8.62855 22.177 3.36707L23 0Z" fill="#006FEE"/>
+        </svg>
+        <form onSubmit={form.handleSubmit(handleLogin)} className="flex w-full items-center max-w-md border rounded-xl bg-white flex-col gap-4 p-4 lg:py-12 lg:px-8">
+            <h1 className="lg:hidden font-bold text-lg">Sign Up</h1>
+            <h1 className="hidden lg:flex font-bold text-3xl">Create an account</h1>
             <div className="flex flex-col w-full gap-1">
                 <label>Name</label>
                 <Input
