a034a098f81f0e2ea721427592a6a17ea03ff9d4
Yousef <yousefsameh24@gmail.com>
2024-05-26 02:12:23 +0300

    Update baseUrl in utils.js to use VITE_BASE_URL environment variable

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,7 +1,7 @@
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export const baseUrl = import.meta.env.MODE === 'development' ? 'http://localhost:3000/api' : '/api';
+export const baseUrl = import.meta.env.VITE_BASE_URL;
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
