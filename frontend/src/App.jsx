ee2417b365627b810d741de0195c4be3d2f53df0
Yousef <yousefsameh24@gmail.com>
2024-05-25 18:05:29 +0300

    Refactor routing logic in App.jsx for better organization and readability

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,50 @@
+import { NextUIProvider } from '@nextui-org/react'
+import { useNavigate, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home'
+import LandingPage from './pages/LandingPage'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Stats from './pages/Stats'
+import AdminPanel from './pages/AdminPanel'
+import { useUserStore } from './stores/user'
+
 function App() {
+  const navigate = useNavigate()
+  const { user } = useUserStore()
+
+  if (!user){
+    return (
+      <NextUIProvider navigate={navigate}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </NextUIProvider>
+    )
+  }
+
+  if (user.admin) {
+    return (
+      <NextUIProvider navigate={navigate}>
+        <Routes>
+          <Route path="/" element={<AdminPanel />} />
+          <Route path="/stats" element={<Stats />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </NextUIProvider>
+    )
+  }
+
   return (
-    <div className="container flex flex-col justify-center items-center">
-      <h1 className="text-3xl">Hello World!</h1>
-    </div>
+    <NextUIProvider navigate={navigate}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </NextUIProvider>
   )
 }
 
