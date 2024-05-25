cd01aa6ad2e83c36736bb536d5a4f63b6c51c41c
Yousef <yousefsameh24@gmail.com>
2024-05-25 20:14:38 +0300

    Add Header component to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Signup from './pages/Signup'
 import Stats from './pages/Stats'
 import AdminPanel from './pages/AdminPanel'
 import { useUserStore } from './stores/user'
+import Header from './components/Header'
 
 function App() {
   const navigate = useNavigate()
@@ -28,22 +29,28 @@ function App() {
   if (user.admin) {
     return (
       <NextUIProvider navigate={navigate}>
+        <div className="flex flex-col min-h-screen">
+        <Header />
         <Routes>
           <Route path="/" element={<AdminPanel />} />
           <Route path="/stats" element={<Stats />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
+        </div>
       </NextUIProvider>
     )
   }
 
   return (
     <NextUIProvider navigate={navigate}>
+      <div className="flex flex-col min-h-screen">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/stats" element={<Stats />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+      </div>
     </NextUIProvider>
   )
 }
