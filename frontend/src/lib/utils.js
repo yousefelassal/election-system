a501ee9e8eee73651254d762943c89530b8877c2
Yousef <yousefsameh24@gmail.com>
2024-05-25 19:43:05 +0300

    Update baseUrl in utils.js to use correct API endpoint

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,7 +1,7 @@
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export const baseUrl = import.meta.env.MODE === 'development' ? 'http://localhost:3001/api' : '/api';
+export const baseUrl = import.meta.env.MODE === 'development' ? 'http://localhost:3000/api' : '/api';
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
