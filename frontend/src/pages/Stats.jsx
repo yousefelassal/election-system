974b219ebe4bf08de92f15e65c0567c1172367fe
Yousef <yousefsameh24@gmail.com>
2024-05-26 00:04:46 +0300

    Update Stats page to display live statistics chart

--- frontend/src/pages/Stats.jsx
+++ frontend/src/pages/Stats.jsx
@@ -1,47 +1,34 @@
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
+import useSWR from 'swr';
+import { baseUrl } from '../lib/utils';
+import { getStats } from '../services/stats';
+import { DonutChart, Legend } from '@tremor/react';
 
-export default function Stats() {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
-
+const Stats = () => {
+  const { data, isLoading } = useSWR(`${baseUrl}/stats`, getStats);
+  if (isLoading) return <div>Loading...</div>;
+  const values = data.map((candidate) => {
+    return {
+      name: candidate.name,
+      value: candidate.votes
+    }
+  })
   return (
-    <>
-      <Button onPress={onOpen}>Open Modal</Button>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-              <ModalBody>
-                <p> 
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
-                  dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. 
-                  Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. 
-                  Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur 
-                  proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Action
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
-  );
+    <div className="flex flex-col w-full h-full items-center justify-center lg:px-32">
+      <div className="flex flex-col w-full bg-white rounded-lg p-4">
+      <h1 className="font-bold w-full text-center text-2xl">Live Statistics Chart</h1>
+      <DonutChart
+        data={values}
+        index="name"
+        colors={['blue', 'cyan', 'indigo', 'violet', 'fuchsia']}
+        variant="pie"
+      />
+      <Legend
+        categories={values.map((candidate) => candidate.name)}
+        colors={['blue', 'cyan', 'indigo', 'violet', 'fuchsia']}
+      />
+      </div>
+    </div>
+  )
 }
+
+export default Stats
