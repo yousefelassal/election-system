4e5ab86d76611f468501c18defe6632d9dd0f565
Yousef <yousefsameh24@gmail.com>
2024-05-25 15:32:32 +0300

    create candidate schema

---
+++ backend/models/candidate.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const candidateSchema = new mongoose.Schema({
+    name: String,
+    votes: Number,
+    party: String,
+    image: String,
+})
+
+candidateSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const Candidate = mongoose.model('Candidate', candidateSchema)
+
+module.exports = Candidate
