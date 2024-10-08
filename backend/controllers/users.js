f83fd43e164cbb447056ecfd2c3c4b3a7c1836fd
Yousef Elassal <yousef.elassal@shezlong.com>
2024-10-08 21:50:05 +0300

    handle enabling and verifying 2fa

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcrypt');
+const speakeasy = require('speakeasy');
+const qrcode = require('qrcode');
 const router = require('express').Router();
 const User = require('../models/user');
 
@@ -16,6 +18,38 @@ router.post('/', async (request, response) => {
 
   const savedUser = await user.save();
   response.status(201).json(savedUser);
+});
+
+router.post('/enable-2fa', async (req, res) => {
+  const secret = speakeasy.generateSecret();
+  const { userId } = req.body
+
+  await User.findByIdAndUpdate(userId, { secret: secret.base32 }, { new: true })
+
+  qrcode.toDataURL(secret.otpauth_url, (err, data_url) => {
+    if (err) {
+      res.status(500).json({ message: 'Error generating QR code' });
+      return;
+    }
+    res.json({ secret: secret.base32, data_url });
+  });
+});
+
+router.post('/verify-2fa', async (req, res) => {
+  const { userId, token } = req.body
+  const user = await User.findById(userId);
+  
+  const isVerified = speakeasy.totp.verify({
+    secret: user.secret,
+    encoding: 'base32',
+    token
+  });
+
+  if (isVerified) {
+    res.json({ success: true });
+  } else {
+    res.status(400).json({ success: false });
+  }
 });
 
 router.get('/', async (request, response) => {
--- backend/models/user.js
+++ backend/models/user.js
@@ -8,6 +8,7 @@ const userSchema = new mongoose.Schema({
         required: true
     },
     passwordHash: String,
+    secret: String,
     admin: {
         type: Boolean,
         default: false
